6294ce83af3867b205f454739c66e02ba342d90d
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-05-28 15:21:29 -0600

    add

---
+++ piezas/__init__.py
@@ -0,0 +1,37 @@
+class Jugador:
+    def __init__(self, color):
+        self.color = color
+
+
+blanco = Jugador('blanco')
+negro = Jugador('negro')
+
+
+class PiezaBase:
+    def __init__(self, jugador, nombre, posicion_inicial, movimientos):
+        self.jugador = jugador
+        self.posicion_actual = posicion_inicial
+        self.movimientos = movimientos
+
+    def mover_pieza(pieza, nombre_movimiento=None):
+        movimiento = pieza['movimientos'][nombre_movimiento]
+
+        if self.jugador.color == 'blancas':
+
+            pieza['posicion_actual']['x'] = (
+                pieza['posicion_actual']['x'] + movimiento['x'])
+
+            pieza['posicion_actual']['y'] = (
+                pieza['posicion_actual']['y'] + movimiento['y'])
+
+            return pieza['posicion_actual']
+
+        if self.jugador.color == 'negro':
+
+            pieza['posicion_actual']['x'] = (
+                pieza['posicion_actual']['x'] - movimiento['x'])
+
+            pieza['posicion_actual']['y'] = (
+                pieza['posicion_actual']['y'] - movimiento['y'])
+
+            return pieza['posicion_actual']
