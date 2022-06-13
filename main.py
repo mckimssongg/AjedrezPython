96d5b9a93835ca5bf48a25f790806de528571b38
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-06-12 19:34:53 -0600

    Revert "cambios"
    This reverts commit 3a77e227ea6ee1a8979f12be3d5a374a96752235.

--- main.py
+++ main.py
@@ -3,34 +3,31 @@ import tkinter as tk
 from tablero.tablero_plantilla import TableroPlantilla
 
 #pantilla_tablero = TableroPlantilla(8, 8)
-# print(pantilla_tablero.mostrar())
-
+#print(pantilla_tablero.mostrar())
 
 class play():
     def __init__(self, cuadro):
         self.cuadro = cuadro
-
+       
         self.ventana = tk.Tk()
         self.ventana.title("Ajedrez")
         self.ventana.iconbitmap("")
         self.ventana.geometry(f"{str(cuadro * 8 )}x{str(cuadro * 8 )}")
-        self.ventana.resizable(0, 0)
+        self.ventana.resizable(0,0)
 
         self.interfaz = tk.Canvas(self.ventana)
-        self.interfaz.pack(fill="both", expand=True)
-
+        self.interfaz.pack(fill="both", expand=True)     
+    
     def __call__(self):
-        self.ventana.mainloop()
+     self.ventana.mainloop()
 
     def crearTablero(self):
-        for i in range(8):
-            for j in range(8):
+        for i in range (8):
+            for j in range (8):
                 if (i+j) % 2 == 0:
-                    self.interfaz.create_rectangle(
-                        i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#FFFFFF")
+                    self.interfaz.create_rectangle(i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#FFFFFF")
                 else:
-                    self.interfaz.create_rectangle(
-                        i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#000000")
+                    self.interfaz.create_rectangle(i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#000000")
 
 
 MotorAjedrez = play(80)
