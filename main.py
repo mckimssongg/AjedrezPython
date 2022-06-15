e2f89dd173ac4ee50dd54bb497f4554c0f9a721b
AnayenciMansillaE <pamebaco2019@gmail.com>
2022-06-15 00:42:17 -0600

    Solucion al bug

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-import tkinter as tk
+import tkinter as tk # Se importa la libreria tkinter
+import posicionesP #importar archivo de posiciones
 
-from tablero.tablero_plantilla import TableroPlantilla
-
-#pantilla_tablero = TableroPlantilla(8, 8)
-#print(pantilla_tablero.mostrar())
-
-class play():
+class ventana():
     def __init__(self, cuadro):
+        self.posicion = posicionesP.juego () #se impora el rchivo con la clase
         self.cuadro = cuadro
+        self.imagenes = {} 
        
-        self.ventana = tk.Tk()
-        self.ventana.title("Ajedrez")
-        self.ventana.iconbitmap("")
-        self.ventana.geometry(f"{str(cuadro * 8 )}x{str(cuadro * 8 )}")
-        self.ventana.resizable(0,0)
+        self.ventana = tk.Tk() # se crea una ventana
+        self.ventana.title("Juego de Ajedrez") # titulo de la ventana
+        self.ventana.iconbitmap("./imagenes/icon.ico/") # icono de la ventana pendiente
+        self.ventana.geometry(f"{str(cuadro * 8 )}x{str(cuadro * 8 )}") # se proporciona la altura y ancho
+        self.ventana.resizable(0,0) #tamaño fijo de la ventana, evita que se agrande o minimice
 
-        self.interfaz = tk.Canvas(self.ventana)
-        self.interfaz.pack(fill="both", expand=True)     
+        self.interfaz = tk.Canvas(self.ventana) # Permite acceder a graficos
+        self.interfaz.pack(fill="both", expand=True)  #para que ocupe toda la ventana   
     
     def __call__(self):
      self.ventana.mainloop()
 
     def crearTablero(self):
-        for i in range (8):
-            for j in range (8):
-                if (i+j) % 2 == 0:
+        cuadradoC= 300
+        for i in range (cuadradoC):
+            for j in range (cuadradoC):
+                if (i+j) % 2 == 0: #0
+                    #0+0 % 2 = 0 no tiene residuo entra al if y al tener residuo su valor sera uno se dirigira al else
                     self.interfaz.create_rectangle(i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#FFFFFF")
-                else:
-                    self.interfaz.create_rectangle(i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#000000")
+                    # 0 *0 , 0*0, 0, 0,
+
+                else: #1
+                    self.interfaz.create_rectangle(i*self.cuadro, j*self.cuadro, (i+1)*self.cuadro, (j+1)*self.cuadro, fill="#58A0AD")
+     
+    def importarpiezas(self):
+        piezas= ["peonN", "peonB", "caballoN", "caballoB", "alfilN", "alfilB", "torreN", "torreB", "reinaN", "reinaB", "reyN", "reyB"]
+        for pieza in piezas:
+            self.imagenes[pieza] = tk.PhotoImage(file="./imagenes/" + pieza + ".png")
+
+    def mostrarPiezas(self): 
+        for indicea, i in enumerate(self.posicion.piezas): #listas
+         for indiceb, j in enumerate(i): #valores de las listas
+           if j != (""): #al cumplirse la condicion de ser diferente de vacio se crea una pieza de lo contrario sigue
+            self.interfaz.create_image(indiceb*self.cuadro, indicea*self.cuadro, image=self.imagenes[j], anchor= "nw")
 
 
-MotorAjedrez = play(80)
-MotorAjedrez.crearTablero()
-
-MotorAjedrez()
+correventana = ventana(83)
+correventana.crearTablero()
+correventana.importarpiezas()
+correventana.mostrarPiezas()
+correventana()
---
+++ posicionesP.py
@@ -0,0 +1,15 @@
+class juego():
+
+    def __init__(self):
+
+        self.piezas = [
+        ["torreN", "caballoN", "alfilN",  "reinaN", "reyN", "alfilN", "caballoN", "torreN"],
+        ["peonN" , "peonN"   , "peonN",    "peonN", "peonN", "peonN",   "peonN"  , "peonN"],
+        [(""), (""), (""), (""), (""), (""), (""), ("")],
+        [(""), (""), (""), (""), (""), (""), (""), ("")],
+        [(""), (""), (""), (""), (""), (""), (""), ("")],
+        [(""), (""), (""), (""), (""), (""), (""), ("")],
+        ["peonB" , "peonB"   , "peonB",    "peonB", "peonB", "peonB",   "peonB"  , "peonB"],
+        ["torreB", "caballoB", "alfilB",  "reinaB", "reyB", "alfilB", "caballoB", "torreB"]
+
+        ]
